3c1aa31cc68304d78c71b396819f580087fc60e9
MeechGit <ajobigwe7@gmail.com>
2025-12-09 01:45:35 +0000

    This is my first script!

---
+++ BasicScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NAME="MeechScript"
+echo "Welcome to the Meech Script"
+echo "This is what we got going on in here"
+echo "Looking around..."
+cat /etc/*release
+echo "All done! Thanks"
