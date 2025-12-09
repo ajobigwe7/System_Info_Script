2f38e4160ac666640d7f32a021278ee765fe8d17
MeechGit <ajobigwe7@gmail.com>
2025-12-09 02:57:00 +0000

    Please let this work

--- BasicScript.sh
+++ BasicScript.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 NAME="MeechScript"
 echo "Welcome to the Meech Script"
+sleep 3
+echo " "
 echo "Let's see what we got going on in here"
+sleep 3
 echo "Looking around..."
+sleep 5
+echo " "
 cat /etc/*release 
 echo " "
 echo "-----------------------------"
