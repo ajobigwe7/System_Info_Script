8dcb0fa003338c407b817779825ef3ca5bf015e1
ajobigwe7 <104232344+ajobigwe7@users.noreply.github.com>
2025-12-08 20:32:48 -0600

    Added a lil razzle dazzle

--- BasicScript.sh
+++ BasicScript.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 NAME="MeechScript"
 echo "Welcome to the Meech Script"
-echo "This is what we got going on in here"
+echo "Let's see what we got going on in here"
 echo "Looking around..."
-cat /etc/*release
-echo "All done! Thanks"
+cat /etc/*release |
+echo " "
+echo "-----------------------------"
+echo " "
+uname --all
+echo " "
+echo "I think I'm done here! Thanks !"
